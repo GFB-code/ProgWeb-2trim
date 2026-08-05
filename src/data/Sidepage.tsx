e56aa3ce21b8b7c6c68db3c12a8239bfa081f86e
Gustavo <gugasubzero@gmail.com>
2026-08-04 22:35:37 -0300

    mudado a localização da pasta pages para funcionar melhor

--- src/data/Sidepage.tsx
+++ src/data/Sidepage.tsx
@@ -3,9 +3,9 @@ export default function Sidepage(){
     return(
         <nav className="flex flex-col min-h-screen max-w-1/8 bg-blue-500 pl-1">
             <Link href={"/"} className="hover:underline w-1">Home</Link>
-            <Link href={"../pages/CadastroLivro"} className="hover:underline">Cadastrar Livro</Link>
-            <Link href={"../pages/CadastroAluno"} className="hover:underline">Cadastrar Aluno</Link>
-            
+            <Link href={"/pages/CadastroLivro"} className="hover:underline">Cadastrar Livro</Link>
+            <Link href={"/pages/CadastroAluno"} className="hover:underline">Cadastrar Aluno</Link>
+            {/* <Link href={"/pages/teste"} className="hover:underline">teste</Link> */}
         </nav>
     )
 }
